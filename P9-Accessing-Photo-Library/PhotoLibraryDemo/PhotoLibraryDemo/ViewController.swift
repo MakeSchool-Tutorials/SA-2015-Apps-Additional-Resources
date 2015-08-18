e2d0a89e087782d8bec21298f135b9d4cb81116c
Samara Trilling <samara.trilling@gmail.com>
2015-08-18 11:22:48 -0700

    getRandomPhoto function working. Todo: make synchronous to avoid lag on first call with permissions

--- P9-Accessing-Photo-Library/PhotoLibraryDemo/PhotoLibraryDemo/ViewController.swift
+++ P9-Accessing-Photo-Library/PhotoLibraryDemo/PhotoLibraryDemo/ViewController.swift
@@ -17,12 +17,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var filterButton: UIButton!
     @IBOutlet weak var saveButton: UIButton!
     
+    var photoAsset : PHAsset = PHAsset()
+    
     func getRandomPhoto() {
+        // Set up the infrastructure for the request.
         let imgManager = PHImageManager.defaultManager()
         var requestOptions = PHImageRequestOptions()
         requestOptions.synchronous = true // This is necessary to return the full image, not just a thumbnail.
         var fetchOptions = PHFetchOptions()
         
+        
+        // Make the request to the phone's photo library.
+        if let fetchResult = PHAsset.fetchAssetsWithMediaType(PHAssetMediaType.Image, options: fetchOptions) {
+            
+            // Make sure fetchResult is not an empty array.
+            if (fetchResult.count > 0) {
+                
+                // Create a random number, then choose the image at that random index.
+                var randomInt = Int(arc4random_uniform(UInt32(fetchResult.count)))
+                let photoAsset = fetchResult[randomInt] as! PHAsset!
+                
+                // Proceed with the request for the full image.
+                imgManager.requestImageForAsset(photoAsset, targetSize: view.frame.size, contentMode: PHImageContentMode.AspectFill, options: requestOptions, resultHandler: { (image, info) in
+                    
+                        // Place the retrieved image in the imageView.
+                        self.imageView.image = image
+                    
+                })
+
+            }
+            
+        }
         
     }
     
@@ -30,6 +55,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        self.getRandomPhoto()
+        
     }
 
     override func didReceiveMemoryWarning() {
