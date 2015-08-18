2c61d5d77c5c9d8d9da5ee962043b9d23e8d1683
Samara Trilling <samara.trilling@gmail.com>
2015-08-18 09:58:47 -0700

    Initial commit for photo library demo

---
+++ P9-Accessing-Photo-Library/PhotoLibraryDemo/PhotoLibraryDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoLibraryDemo
+//
+//  Created by Samara Trilling on 8/18/15.
+//  Copyright (c) 2015 Samara Trilling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
